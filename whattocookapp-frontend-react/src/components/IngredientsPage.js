b9f880251043703b1c2352d62a7ce29a2dc4dc05
ldsc117 <62885313+ldsc117@users.noreply.github.com>
2021-06-29 14:20:14 +0300

    Made a json structure and file for ingredients with future use in Ingredients page

--- whattocookapp-frontend-react/src/components/IngredientsPage.js
+++ whattocookapp-frontend-react/src/components/IngredientsPage.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import ingredients from '../ingredients.json';
+
 
 export default function IngredientsPage() {
+
+    const ingredientList = ingredients;
+    
+    console.log(ingredientList);
     return (
         <div>
-            <h1>Ingredients</h1>
+            <h1>Ingredients ingred</h1>
+            <div className="ingredients-card-container">
+            
+            </div>
         </div>
     )
 }
